e6d0afb24e88c4b74b50b8d5ea4ccaacb08ef96b
ABskr <bskr26@gmx.de>
2021-04-28 16:10:01 +0200

    add CRUD for 'reviews'

--- src/services/articles/index.js
+++ src/services/articles/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import q2m from 'query-to-mongo'
 
 import ArticleModel from './schema.js'
 
@@ -6,8 +8,19 @@ const articlesRouter = express.Router()
 
 articlesRouter.get('/', async (req, res, next) => {
   try {
-    const articles = await ArticleModel.find()
-    res.send(articles)
+    // const articles = await ArticleModel.find()
+    // res.send(articles)
+    const query = q2m(req.query)
+    //console.log(query)
+    const totalArticles = await ArticleModel.countDocuments(query.criteria)
+    // console.log(totalArticles)
+    
+    const articles = await ArticleModel.find(query.criteria, query.options.fields)
+    .skip(query.options.skip)
+    .limit(query.options.limit)
+    .sort(query.options.sort)
+    
+    res.send({ links: query.links('/articles', totalArticles), articles})
   } catch (error) {
     console.log(error)
     next(error)
@@ -62,6 +75,111 @@ articlesRouter.delete('/:id', async (req, res, next) => {
     } else {
       res.status(404).send(`User with id ${req.params.id} not found`)
     }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+articlesRouter.get('/:articleId/reviews', async (req, res, next) => {
+  try {
+    const { reviews } = await ArticleModel.findById(req.params.articleId,{ _id: 0})
+    res.send(reviews)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+articlesRouter.get('/:articleId/reviews/:reviewId', async (req, res, next) => {
+  try {
+    const { reviews } = await ArticleModel.findOne(
+      {
+        _id: mongoose.Types.ObjectId(req.params.articleId)
+      },
+      {
+        reviews: {
+          $elemMatch: {
+            _id: mongoose.Types.ObjectId(req.params.reviewId)
+          }
+        }
+      }
+    )
+
+    if (reviews && reviews.length > 0) {
+      res.send(reviews[0])
+    } else {
+      const error = new Error()
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+articlesRouter.post('/:articleId', async (req, res, next) => {
+  try {
+    const updatedArticle = await ArticleModel.findByIdAndUpdate(
+      req.params.articleId,
+      {
+        $push: {
+          reviews: req.body
+        }
+      },
+      { runValidators: true, new:true }
+    )
+    res.send(updatedArticle)
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+articlesRouter.put('/:articleId/reviews/:reviewId', async (req, res, next) => {
+  try {
+    const moddedArticle = await ArticleModel.findOneAndUpdate(
+      {
+        _id: mongoose.Types.ObjectId(req.params.articleId),
+        "reviews._id": mongoose.Types.ObjectId(req.params.reviewId)
+      },
+      {
+        $set: { 'reviews.$': req.body}
+      },
+      {
+        runValidators: true,
+        new: true
+      }
+    )
+    if (moddedArticle) {
+      res.send(moddedArticle)
+    } else {
+      const error = new Error()
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
+articlesRouter.delete('/:articleId/reviews/:reviewId', async (req, res, next) => {
+  try {
+    const modifiedArticle = await ArticleModel.findByIdAndUpdate(
+      req.params.articleId,
+      {
+        $pull: {
+          reviews: 
+            {
+              _id: mongoose.Types.ObjectId(req.params.reviewId)
+            }
+        }
+      },
+      {new: true}
+    )
+    res.send(modifiedArticle)
   } catch (error) {
     console.log(error)
     next(error)
